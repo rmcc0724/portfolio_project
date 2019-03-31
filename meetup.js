72eed77a5e5baeb879861223f94ddeb0004026e3
Reid <rmcc0724@gmail.com>
2019-03-31 16:26:45 -0700

    Homepage update

---
+++ meetup.js
@@ -0,0 +1,24 @@
+function problem2(...args) {
+
+    //1. sort the array in ascending order
+
+    //2. store the 1st, last and middle element as the targets i, j, k
+
+    //3. go through indexes j and k
+
+    //4. if not true start moving i forward
+};
+
+problem2([2, 3, -1, 9, -4, 8, 12], 17);
+
+-4, -1, 2, 3, 8, 9, 12,
+
+
+//3. Map any number from 0 to 9999 to words
+    //a. You need to map out each possibility
+    //b. 1-20, 30, etc, 100, 200, etc, 1000, 2000, etc
+    //c. check the length of the number
+    //d. divide it by 10 if 60, 100 if 500 or 1000 if 5000 etc
+    //e. send the result to a sub function that converts it to words
+    //f. use modulus to send the remainder andconvert it the same way
+//Raju Manchala
